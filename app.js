c98edd17c6408d65d951fd560deefa9986511e7e
Ashley <ahappyday4all@gmail.com>
2021-06-14 23:28:10 +0800

    feat: Add restaurant index page

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app) // 載入的是一個函式，app 是要傳入函式的參數
 
 module.exports = app
