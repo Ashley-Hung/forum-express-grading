b4fe3df6d9835eda619076d2474a3c5403efd334
Ashley <ahappyday4all@gmail.com>
2021-06-15 11:08:08 +0800

    feat: Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
@@ -8,6 +10,14 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: true }))
+app.use(flash())
+
+app.use((req, res, next) => {
+	res.locals.success_msg = req.flash('success_msg')
+	res.locals.error_msg = req.flash('warning_msg')
+	next()
+})
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -6,14 +6,41 @@ const userController = {
 	signUpPage: (req, res) => {
 		return res.render('signup')
 	},
+
 	signUp: (req, res) => {
-		const { name, email, password } = req.body
-		User.create({
-			name,
-			email,
-			password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-		}).then(user => {
-			return res.redirect('/signin')
+		const { name, email, password, passwordCheck } = req.body
+		const errors = []
+		if (!name || !email || !password || !passwordCheck) {
+			errors.push({ message: '所有欄位皆為必填' })
+		}
+
+		if (password !== passwordCheck) {
+			errors.push({ message: '密碼與確認密碼不相符！' })
+		}
+
+		if (errors.length) {
+			return res.render('signup', { errors, name, email, password })
+		}
+
+		User.findOne({ where: { email } }).then(user => {
+			if (user) {
+				errors.push({ message: '這個 Email 已經註冊過了。' })
+				return res.render('signup', { errors, name, email, password })
+			}
+
+			return User.create({
+				name,
+				email,
+				password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+			})
+				.then(() => {
+					req.flash('success_msg', '成功註冊帳號！')
+					return res.redirect('/signin')
+				})
+				.catch(err => {
+					res.end('An unexpected error has occurred')
+					console.log(err)
+				})
 		})
 	}
 }
