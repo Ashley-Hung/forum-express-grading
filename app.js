1de6d51c7a65171db3ce3ff2914fb463db7f5496
Ashley <ahappyday4all@gmail.com>
2021-06-15 10:12:55 +0800

    feat: Add userController and signup page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { User } = db
+
+const userController = {
+	signUpPage: (req, res) => {
+		return res.render('signup')
+	},
+	signUp: (req, res) => {
+		const { name, email, password } = req.body
+		User.create({
+			name,
+			email,
+			password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+		}).then(user => {
+			return res.redirect('/signin')
+		})
+	}
+}
+
+module.exports = userController
