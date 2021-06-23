c1920e0332d738751b57212dd2e3ea5ef5b7e631
Ashley <ahappyday4all@gmail.com>
2021-06-23 12:07:05 +0800

    feat: add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,26 +1,51 @@
 const { Restaurant, Category } = require('../models')
+const pageLimit = 10
 
 const restController = {
   getRestaurants: async (req, res, next) => {
+    let offset = 0
     const whereQuery = {}
-    if (req.query.categoryId) {
-      whereQuery.CategoryId = Number(req.query.categoryId)
-    }
-    console.log(whereQuery)
+    if (req.query.page) offset = (req.query.page - 1) * pageLimit
+    if (req.query.categoryId) whereQuery.CategoryId = Number(req.query.categoryId)
 
     try {
-      const [restaurants, categories] = await Promise.all([
-        Restaurant.findAll({ include: Category, where: whereQuery, raw: true, nest: true }),
+      const [result, categories] = await Promise.all([
+        Restaurant.findAndCountAll({
+          include: Category,
+          where: whereQuery,
+          offset,
+          limit: pageLimit,
+          raw: true,
+          nest: true
+        }),
         Category.findAll({ raw: true, nest: true })
       ])
 
-      const data = restaurants.map(r => ({
+      // data for pagination
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(result.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+
+      const data = result.rows.map(r => ({
         ...r,
         description: r.description.substring(0, 50),
         categoryName: r.Category.name
       }))
 
-      return res.render('restaurants', { restaurants: data, categories, categoryId: whereQuery.CategoryId })
+      console.log('==================================================')
+      console.log(data)
+      return res.render('restaurants', {
+        restaurants: data,
+        categories,
+        categoryId: whereQuery.CategoryId,
+        pages: pages <= 1 ? 'invisible' : '', // <= 一頁不顯示 pagination
+        page,
+        totalPage,
+        prev,
+        next
+      })
     } catch (error) {
       next(error)
     }
